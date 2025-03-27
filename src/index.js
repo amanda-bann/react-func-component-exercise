709c9a40ae9a3d0d7db4b797ff991de4097d1f40
amanda-bann <amanda.banner@gmail.com>
2025-03-26 19:06:36 -0600

    Pass props to your functional component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag";
-
 import "./styles.css";
 
 const App = () => (
   <div className="App">
     <h1>Name Tag Generator</h1>
-    <NameTag />
+    <NameTag name="Teklanika" />
+    <NameTag name="Denali" />
+    <NameTag name="Foraker" />
+    <NameTag name="Hunter" />
+    <NameTag name="Wickersham" />
+    <NameTag name="Matanuska" />
+    <NameTag name="Knik" />
+    <NameTag name="Talkeetna" />
+    <NameTag name="Susitna" />
   </div>
 );
 
