78ebee93739c3bd34d90af01a5fb986b4eb2da4e
amanda-bann <amanda.banner@gmail.com>
2025-04-01 16:25:55 -0600

    Convert from functional to stateful components

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag.js";
-import { names } from "./data.js";
 import "./styles.css";
 
 const renderNameTag = (name) => <NameTag name={name.person} key={name.id} />;
 
-const App = () => {
-  const NameTagElements = names.map(renderNameTag);
-  return (
-    <div className="App">
-      <h1>Name Tag Generator</h1>
-      {NameTagElements}
+class App extends Component{
+  state = { names: ["Teklanika", "Denali", "K2", "Everest", "Hunter", "Wickersham"] };
+  render () {
+    const NameTagElements = this.state.names.map(renderNameTag);
+    return (
+      <div className="App">
+        <h1>Name Tag Generator</h1>
+        {NameTagElements}
       </div>
-  );
-};
+    );
+  }
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
